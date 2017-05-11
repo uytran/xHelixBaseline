6c326d41b6c2bc76b8b1358a449f3adb11bff6ba
Uy Tran <uytran@gmail.com>
2017-05-11 09:52:10 -0700

    Add project files.

---
+++ README.md
@@ -0,0 +1,3 @@
+#Xcentium xHelix
+
+xHelix is a Sitecore starter package base off of Sitecore's Helix and Habitat
---
+++ gulp-config.js
@@ -0,0 +1,15 @@
+module.exports = function () {
+  var instanceRoot = "C:\\websites\\Habitat.dev.local";
+  var config = {
+    websiteRoot: instanceRoot + "\\Website",
+    sitecoreLibraries: instanceRoot + "\\Website\\bin",
+    licensePath: instanceRoot + "\\Data\\license.xml",
+    solutionName: "Habitat",
+    buildConfiguration: "Debug",
+    buildPlatform: "Any CPU",
+    buildToolsVersion: 15.0, //change to 15.0 for VS2017 support
+    publishPlatform: "AnyCpu",
+    runCleanBuilds: false
+  };
+  return config;
+}
